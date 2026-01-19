5ad21e94d531a8739191477ba3056ffb0c26e9ce
ishpreetsidhu13 <ishpreetsidhu13@gmail.com>
2026-01-18 23:38:14 -0500

    add arrays and generate random pet

--- lab-1/script.js
+++ lab-1/script.js
@@ -3,3 +3,15 @@ let petName = "";
 let petType = "";
 let petAge = 0;
 
+// Arrays
+const types = ["dog", "cat", "dragon", "hamster"];
+const names = ["Luna", "Max", "Bella", "Charlie", "Daisy", "Buddy"];
+// Generate random pet
+function generatePet() {
+    petName = names[Math.floor(Math.random() * names.length)];
+    petType = types[Math.floor(Math.random() * types.length)];
+    petAge = Math.floor(Math.random() * 10) + 1;
+    
+    updateDisplay();
+}
+// Update display
