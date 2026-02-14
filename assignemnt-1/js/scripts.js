aa21f608366c7cc5e494fb1d89aa1ffe704f7532
ishpreetsidhu13-dev <ishpreetsidhu13@gmail.com>
2026-02-13 22:58:08 -0500

    story words

---
+++ assignemnt-1/js/scripts.js
@@ -0,0 +1,9 @@
+
+// Story words
+const words = {
+    part1: ["The turkey", "The dog", "My teacher", "The elephant"],
+    part2: ["sat on", "danced with", "ate", "saw"],
+    part3: ["a funny", "a scary", "a goofy", "a slimy"],
+    part4: ["monkey", "frog", "bug", "worm"],
+    part5: ["on the moon", "in my soup", "on the grass", "in my shoes"]
+};
