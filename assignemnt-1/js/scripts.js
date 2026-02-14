3cb615d01a69e9f3215e36decd1a74d1c8d490b5
ishpreetsidhu13-dev <ishpreetsidhu13@gmail.com>
2026-02-13 23:03:40 -0500

    positions

--- assignemnt-1/js/scripts.js
+++ assignemnt-1/js/scripts.js
@@ -7,3 +7,26 @@ const words = {
     part4: ["monkey", "frog", "bug", "worm"],
     part5: ["on the moon", "in my soup", "on the grass", "in my shoes"]
 };
+
+// position
+let currentIndex = {
+    part1: 0,
+    part2: 0,
+    part3: 0,
+    part4: 0,
+    part5: 0
+};
+
+const partButtons = [
+    document.getElementById("part1"),
+    document.getElementById("part2"),
+    document.getElementById("part3"),
+    document.getElementById("part4"),
+    document.getElementById("part5")
+];
+
+const showStoryBtn = document.getElementById("showStory");
+const randomStoryBtn = document.getElementById("randomStory");
+const resetBtn = document.getElementById("reset");
+const storyOutput = document.getElementById("storyOutput");
+
