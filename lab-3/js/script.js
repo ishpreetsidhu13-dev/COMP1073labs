526ae02aa2e36dcd54b3a924320609005afc0028
ishpreetsidhu13-dev <ishpreetsidhu13@gmail.com>
2026-03-17 01:50:50 -0400

    car create in js

---
+++ lab-3/js/script.js
@@ -0,0 +1,10 @@
+//Car Creator
+
+let cars = [];
+let nextId = 1;
+const COLORS = ['Red', 'Blue', 'Green'];
+const MATERIALS = ['Metal', 'Plastic'];
+
+const form = document.getElementById('toyCarForm');
+const randomBtn = document.getElementById('randomBtn');
+const carsUl = document.getElementById('carsUl');
