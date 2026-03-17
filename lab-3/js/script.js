93916bc8da92ee0547dd975f1509c751bfccb5b0
ishpreetsidhu13-dev <ishpreetsidhu13@gmail.com>
2026-03-17 01:51:59 -0400

    add events listner

--- lab-3/js/script.js
+++ lab-3/js/script.js
@@ -8,3 +8,9 @@ const MATERIALS = ['Metal', 'Plastic'];
 const form = document.getElementById('toyCarForm');
 const randomBtn = document.getElementById('randomBtn');
 const carsUl = document.getElementById('carsUl');
+// Events
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createToyCar();
+});
+randomBtn.addEventListener('click', generateRandomCar);
