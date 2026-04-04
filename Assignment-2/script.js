346ce7e1635612a73696337c413efe3e023149e5
ishpreetsidhu13-dev <ishpreetsidhu13@gmail.com>
2026-04-03 23:15:04 -0400

    add get elements and task button

---
+++ Assignment-2/script.js
@@ -0,0 +1,13 @@
+// get element
+let taskInput = document.getElementById("taskInput");
+let addButton = document.getElementById("addBtn");
+let taskList = document.getElementById("taskList");
+
+
+let myTasks = [];
+
+// add task button
+addButton.addEventListener("click", addNewTask);
+
+
+;
