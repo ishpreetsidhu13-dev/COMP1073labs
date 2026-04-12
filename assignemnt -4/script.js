cdfab4df2d2d19c15e5323dccc3cdf888226a41b
Harmanjeet singh <harmannjeet.singh7@gmail.com>
2026-04-11 21:07:36 -0400

    selected all required elements in javascript

---
+++ assignemnt -4/script.js
@@ -0,0 +1,14 @@
+//api key and urls
+const API_KEY = "60NpVcYw6CrjTJT75I3effl4PmmnOOSk";
+const searchUrl = "https://api.giphy.com/v1/gifs/search";
+const trendingUrl = "https://api.giphy.com/v1/gifs/trending";
+
+//  elements
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#searchBtn");
+const trendingBtn = document.querySelector("#trendingBtn");
+const statusMessage = document.querySelector("#statusMessage");
+const featuredResult = document.querySelector("#featuredResult");
+const results = document.querySelector("#results");
+
+
